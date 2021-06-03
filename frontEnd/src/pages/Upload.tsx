238f1737fd543862d486f90a340a3968457a6101
Gabriela Ávila <magaby.tec@gmail.com>
2021-06-03 17:29:29 -0600

    adding upload files view

--- frontEnd/src/pages/Upload.tsx
+++ frontEnd/src/pages/Upload.tsx
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import Path from 'path';
-import uploadFileToBlob, { isStorageConfigured } from './storage-blob';
+import { Input, Button, Typography, Dialog, DialogActions, 
+  DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
+import uploadFileToBlob from './storage-blob';
 import * as contName from './storage-blob';
 
-const storageConfigured = isStorageConfigured();
-const acc = localStorage.getItem("account");
-const listDocument: string[] = [];
+var SAS = "";
+const storageConfigured = true;
 let userA = localStorage.getItem("account");
 export var containerName = (userA || '');
 
 const UploadDocument = (): JSX.Element => {
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
   useEffect(() => {
     async function getLink() {
       var Hearders = {
@@ -22,7 +26,8 @@ const UploadDocument = (): JSX.Element => {
       headers : new Headers (Hearders)
     });
     const json = await response.json();
-    console.log(json);
+    SAS = json[0].sas;
+    console.log(SAS);    
     }
     getLink();
   }, []);
@@ -37,8 +42,6 @@ const UploadDocument = (): JSX.Element => {
   const [uploading, setUploading] = useState(false);
   const [inputKey, setInputKey] = useState(Math.random().toString(36));
 
-  const [ hasError, setHasError ] = useState(false);
-
   const onFileChange = (event: any) => {
     // capture file into state
     setFileSelected(event.target.files[0]);
@@ -46,25 +49,21 @@ const UploadDocument = (): JSX.Element => {
 
   async function post(file : any) {
     try {
-      // http://localhost:9080/Gradle__com_example__api_1_0_SNAPSHOT_war/api/storage/{'+contName.containerName+': '', '+impFileName+': ''}'
-        // Create request to api service
         console.log("Account: " + localStorage.getItem("account"));
         console.log("Container: " + contName.containerName);
-        const req = await fetch('http://localhost:9080/Gradle___com_example___api_1_0_SNAPSHOT_war/api/storage/{\'containerName\': '+ contName.containerName +', \'filename\': '+ file +'}', {
-            /*
-            method: 'POST',
-            headers: { 'Content-Type':'application/json' },
-            
-            // format the data
-            body: JSON.stringify({
-
-            }),*/
-        });
-        
-        const res = await req.json();
-
-        // Log success message
-        console.log(res);                
+        let test = await fetch('http://localhost:4000/Upload', {
+                method: 'GET',
+                headers : {
+                Accept: '*/*',
+                'Content-Type': 'text/plain',
+                documentName: file,
+                }});
+        const json = await test.json(); 
+        console.log(json); 
+        if (json[0].status == "ok"){
+          console.log("aqui")
+          setOpen(true);
+        }          
     } catch(err) {
         console.error(`ERROR: ${err}`);
     }
@@ -74,15 +73,13 @@ const UploadDocument = (): JSX.Element => {
     setUploading(true);
 
     // *** UPLOAD TO AZURE STORAGE ***
-    const blobsInContainer: string[] = await uploadFileToBlob(fileSelected);
+    const blobsInContainer: string[] = await uploadFileToBlob(fileSelected, SAS);
     
     // prepare UI for results
     setBlobList(blobsInContainer);
 
-    //post(blobsInContainer[blobsInContainer.length - 1].split(/(\\|\/)/g).pop());
     console.log("marcador: " + localStorage.getItem('nameFile'));
     post(localStorage.getItem('nameFile'));
-    //console.log("Nombre del archivo: " + blobsInContainer[blobsInContainer.length - 1].split(/(\\|\/)/g).pop());
 
     // reset state/form
     setFileSelected(null);
@@ -93,41 +90,38 @@ const UploadDocument = (): JSX.Element => {
   // display form
   const DisplayForm = () => (
     <div>
-      <input type="file" onChange={onFileChange} key={inputKey || ''} />
-      <button type="submit" onClick={onFileUpload}>
-        Upload!
-          </button>
+      <Input type="file" color="primary" onChange={onFileChange} key={inputKey || ''} />
+      <p />
+      <Button variant="contained" color="primary" onClick={onFileUpload}>
+        Upload
+      </Button>
     </div>
   )
-
-  // display file name
-  const DisplayNameFileFromContainer = () => (
-    <div>
-      <h2>Container items</h2>
-      <ul>
-        {blobList.map((item) => {
-          //listDocument = blobList;
-          return (
-            <li key={item}>
-              <div>
-                {Path.basename(item)}
-                <br />
-              </div>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
-
   return (
     <div>
-      <h1>Upload file to Analize</h1>
+      <Typography variant="h4" >Upload file to analize</Typography>
+      <hr />  
       {storageConfigured && !uploading && DisplayForm()}
-      {storageConfigured && uploading && <div>Uploading</div>}
-      <hr />
-      {storageConfigured && blobList.length > 0 && DisplayNameFileFromContainer()}
-      {!storageConfigured && <div>Storage is not configured.</div>}
+      {storageConfigured && uploading && <Typography variant = "h6">Uploading...</Typography>}
+      
+      <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+            >
+            <DialogTitle id="alert-dialog-title">{"Successful"}</DialogTitle>
+              <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                  File uploaded successfully!
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose} color="primary">
+                    Accept
+                </Button>
+              </DialogActions>
+      </Dialog>
     </div>
   );
 };
--- frontEnd/src/pages/storage-blob.ts
+++ frontEnd/src/pages/storage-blob.ts
@@ -1,59 +1,12 @@
 import { BlobServiceClient, ContainerClient} from '@azure/storage-blob';
 
+//const sasToken = process.env.storagesastoken || "se=2021-06-03T05%3A28%3A49Z&sp=rwl&sv=2020-06-12&ss=b&srt=sco&sig=jHDxYT37Z6Vegt0%2B0sbF2yQoO75%2BV6ni6rqZ%2BnK5VLQ%3D"; // Fill string with your SAS token
+var sasToken = process.env.storagesastoken || "test";
+let user = localStorage.getItem("account")?.toLowerCase();
 
-const sasToken = process.env.storagesastoken || "sv=2020-02-10&ss=bfqt&srt=sco&sp=rwdlacupx&se=2021-04-24T05:47:07Z&st=2021-04-23T21:47:07Z&spr=https,http&sig=lAL%2BkPnSvQlvvO%2BUmasZBrnt2uERNYA%2F3UUJJSSi8jg%3D"; // Fill string with your SAS token
-let a = localStorage.getItem("account")?.toLowerCase();
-console.log("min: " + a);
-export var containerName = (a || '');
-// account name as the name of the container
-//localStorage.setItem('account', localStorage.getItem("account"));
-//console.log("imp: " + localStorage.getItem("account"));
-//export const containerName = (a || '');
-console.log("2: " + containerName);
-const storageAccountName = process.env.storageresourcename || "soadocument"; // Fill string with your Storage resource name
-// </snippet_package>
+export var containerName = (user || '');
+const storageAccountName = process.env.storageresourcename || "proyectosoa"; // Fill string with your Storage resource name
 
-// <snippet_isStorageConfigured>
-// Feature flag - disable storage feature to app if not configured
-export const isStorageConfigured = () => {
-  return (!storageAccountName || !sasToken) ? false : true;
-}
-// </snippet_isStorageConfigured>
-
-// <snippet_getBlobsInContainer>
-// return list of blobs in container to display
-const getBlobsInContainer = async (containerClient: ContainerClient) => {
-  const returnedBlobUrls: string[] = [];
-
-  // get list of blobs in container
-  // eslint-disable-next-line
-  for await (const blob of containerClient.listBlobsFlat()) {
-    // if image is public, just construct URL
-    returnedBlobUrls.push(
-      `https://${storageAccountName}.blob.core.windows.net/${containerName}/${blob.name}`
-    );
-  }
-  return returnedBlobUrls;
-}
-
-export const getBlobNames = async (contName: string) => {
-  const blobService = new BlobServiceClient(
-    `https://${storageAccountName}.blob.core.windows.net/?${sasToken}`
-  );
-  const containerClient: ContainerClient = blobService.getContainerClient(contName);
-  const blobNames = [];
-
-  // get list of blobs in container
-  for await (const blob of containerClient.listBlobsFlat()) {
-    // if image is public, just construct URL
-    blobNames.push(blob.name);
-  }
-  return blobNames;
-}
-
-// </snippet_getBlobsInContainer>
-
-// <snippet_createBlobInContainer>
 const createBlobInContainer = async (containerClient: ContainerClient, file: File) => {
   
   // create blobClient for container
@@ -70,31 +23,23 @@ export const impFileName = async (file: File | null): Promise<string> => {
   if (!file) return "";
   return file.name;
 }
-// </snippet_createBlobInContainer>
 
-// <snippet_uploadFileToBlob>
-const uploadFileToBlob = async (file: File | null): Promise<string[]> => {
+const uploadFileToBlob = async (file: File | null, sas: string): Promise<string[]> => {
+  console.log("entre");
+  sasToken = sas;
+  console.log(sasToken);
   if (!file) return [];
   impFileName(file);
   console.log("...: " + file.name);
   localStorage.setItem('nameFile', file.name);
-
-  // get BlobService = notice `?` is pulled out of sasToken - if created in Azure portal
   const blobService = new BlobServiceClient(
     `https://${storageAccountName}.blob.core.windows.net/?${sasToken}`
   );
 
-  // get Container - full public read access
   const containerClient: ContainerClient = blobService.getContainerClient(containerName);
-  await containerClient.createIfNotExists({
-    access: 'container',
-  });
-
-  // upload file
   await createBlobInContainer(containerClient, file);
 
-  // get list of blobs in container
-  return getBlobsInContainer(containerClient);
+  return []
 };
 // </snippet_uploadFileToBlob>
 
