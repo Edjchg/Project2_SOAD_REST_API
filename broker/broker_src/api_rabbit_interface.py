ecfdb87174b0fb8a6e15e51b9de0ae92d6b91472
Karla Rivera <karla.rivera24@gmail.com>
2021-06-06 23:05:19 -0600

    Adding new branch

--- broker/broker_src/api_rabbit_interface.py
+++
@@ -1,8 +0,0 @@
-import pika
-
-class API_RMQ_interface():
-    def __init__(self, host):
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
-        self.channel = self.connection.channel()
-        self.queues = ['nlp', 'nlp_response', 'storage', 'storage_response']
-
